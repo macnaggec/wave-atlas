8352cbe243aec0ea30bd982a61e751ee2fe89a3d
chevylevel <chevylevel@gmail.com>
2026-06-20 17:50:50 +0300

    refactor(widgets): remove dead collection panel

--- src/entities/SurfSession/index.ts
+++ src/entities/SurfSession/index.ts
@@ -1,7 +1,6 @@
 export type { SurfSessionItem, SurfSessionPage } from './types';
 export type { SessionFeedFilter } from './model/useSessionFeed';
 
-export { useMySessions } from './model/useMySessions';
 export { usePublishSession } from './model/usePublishSession';
 export { useSessionFeed } from './model/useSessionFeed';
 export { useSessionMedia } from './model/useSessionMedia';
